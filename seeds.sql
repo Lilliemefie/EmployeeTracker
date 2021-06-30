0295790ae0b531bf9c3640034635974f1e6a0bda
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-29 21:40:29 -0700

    deleted package.json and reinstalled

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Alice", "Park", 1, 1);
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Engineer", "40,000", 1);
+
+
+INSERT INTO department (names)
+VALUES ("Engineering"),
+("Mechanical"),
+("Management"),
+("Tech Support"),
+
+
+
---
+++ server.js
@@ -0,0 +1,47 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const console = require('console.table');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: 'root',
+  // Your password
+  password: 'Ucla2021$',
+  database: 'company_DB',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    runSearch();
+  });
+
+  const runSearch = () => {
+      inquirer
+      .prompt({
+          type: 'list',
+          message: 'What would you like to do?',
+          name: 'action',
+          choices: [
+              'View all employee',
+              'View all departments',
+              'View all roles',
+              'Add employee',
+              'Add departments',
+              'Add roles',
+              'Update employee role',
+              'Update employee managers',
+              'View employee by manager'
+          ]
+      })
+      .then((answer) => {
+          switch (answer.choice){
+            case 'View all employee':
+                viewAllEmployee();
+          }
+      })
+  }
+  runSearch();
